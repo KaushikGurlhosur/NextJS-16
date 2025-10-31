60c25b6b377fd143381b9a4f5a998e3612bc6b7d
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2025-10-31 17:24:05 +0530

    dynamic route - [id] for users/[id]

---
+++ app/dashboard/users/[id]/page.tsx
@@ -0,0 +1,7 @@
+const UserDetails = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const { id } = await params;
+
+  return <div>Showing details for user #{id}</div>;
+};
+
+export default UserDetails;
