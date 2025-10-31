58be108c8bc57f281b561ad7a6bd119e008543fd
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2025-10-31 18:04:07 +0530

    docs: NextJS 16 routing enhancements (layout deduplication and incremental prefetching) added to the Readme.MD file - Theory explaination

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   console.log("nextjs 16");
   return (
     <main>
-      <h1>NextJS 16</h1>
+      <h1>Welcome to NextJS 16</h1>
       <Hello />
     </main>
   );
